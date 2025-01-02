b81aa28635411a2afa4aefb259d2d81a428aa878
Raj Nitin Gada <rgada28@gmail.com>
2025-01-02 10:44:10 +0530

    feat: Add iOS isDeviceRooted and isEmulator check

--- ios/Classes/MobileGarudaPlugin.swift
+++ ios/Classes/MobileGarudaPlugin.swift
@@ -1,5 +1,6 @@
 import Flutter
 import UIKit
+import DTTJailbreakDetection
 
 public class MobileGarudaPlugin: NSObject, FlutterPlugin {
   public static func register(with registrar: FlutterPluginRegistrar) {
@@ -12,8 +13,47 @@ public class MobileGarudaPlugin: NSObject, FlutterPlugin {
     switch call.method {
     case "getPlatformVersion":
       result("iOS " + UIDevice.current.systemVersion)
+     case "isDeviceRooted":
+            result(isJailBroken())
+            break
+     case "isDeviceSafe":
+            result(false)
+            break
+      case "isDebuggingModeEnable":
+            result(false)
+            break
+      case  "isDeveloperModeEnabled":
+            result(false)
+            break
+      case "isEmulator":
+            result(isSimulator())
+            break
+      case "enableScreenshot":
+            result(false)
+            break
+      case "disableScreenshot":
+            result(false)
+            break
+      case "isDebuggerAttached":
+            result(false)
+            break
+      case "isAppCloned":
+            result(false)
+            break
     default:
       result(FlutterMethodNotImplemented)
     }
   }
+
+  private func isSimulator() -> Bool {
+         #if targetEnvironment(simulator)
+             return true
+         #else
+            return false
+         #endif
+    }
+
+   private func isJailBroken() -> Bool {
+        return DTTJailbreakDetection.isJailbroken()
+    }
 }
