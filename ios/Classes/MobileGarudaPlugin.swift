afec668c7b5f183893bcd5245adac7d56184b9a4
Raj Nitin Gada <rgada28@gmail.com>
2025-01-03 13:44:44 +0530

    feat: Add ios DebuggerAttached detection

--- ios/Classes/MobileGarudaPlugin.swift
+++ ios/Classes/MobileGarudaPlugin.swift
@@ -35,7 +35,7 @@ public class MobileGarudaPlugin: NSObject, FlutterPlugin {
             screenProtectorKit.enabledPreventScreenshot()
             result(true)
         case "isDebuggerAttached":
-            result(false)
+            result(isDebuggerAttached)
         case "isAppCloned":
             result(false)
         default:
@@ -55,26 +55,26 @@ public class MobileGarudaPlugin: NSObject, FlutterPlugin {
         return DTTJailbreakDetection.isJailbroken()
     }
     
-    // let isDebuggerAttached: Bool = {
-    //     var debuggerIsAttached = false
+    let isDebuggerAttached: Bool = {
+        var debuggerIsAttached = false
     
-    //     var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
-    //     var info: kinfo_proc = kinfo_proc()
-    //     var info_size = MemoryLayout<kinfo_proc>.size
+        var name: [Int32] = [CTL_KERN, KERN_PROC, KERN_PROC_PID, getpid()]
+        var info: kinfo_proc = kinfo_proc()
+        var info_size = MemoryLayout<kinfo_proc>.size
     
-    //     let success = name.withUnsafeMutableBytes { (nameBytePtr: UnsafeMutableRawBufferPointer) -> Bool in
-    //         guard let nameBytesBlindMemory = nameBytePtr.bindMemory(to: Int32.self).baseAddress else { return false }
-    //         return -1 != sysctl(nameBytesBlindMemory, 4, &info, &info_size, nil, 0)
-    //     }
+        let success = name.withUnsafeMutableBytes { (nameBytePtr: UnsafeMutableRawBufferPointer) -> Bool in
+            guard let nameBytesBlindMemory = nameBytePtr.bindMemory(to: Int32.self).baseAddress else { return false }
+            return -1 != sysctl(nameBytesBlindMemory, 4, &info, &info_size, nil, 0)
+        }
     
-    //     if !success {
-    //         debuggerIsAttached = false
-    //     }
+        if !success {
+            debuggerIsAttached = false
+        }
     
-    //     if !debuggerIsAttached && (info.kp_proc.p_flag & P_TRACED) != 0 {
-    //         debuggerIsAttached = true
-    //     }
+        if !debuggerIsAttached && (info.kp_proc.p_flag & P_TRACED) != 0 {
+            debuggerIsAttached = true
+        }
     
-    //     return debuggerIsAttached
-    // }()
+        return debuggerIsAttached
+    }()
 }
